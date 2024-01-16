46f94911cc30efa748c82e870a4e5932e48c85f9
danilkberlin <danilkravets19@gmail.com>
2024-01-16 13:13:45 +0100

    Мы создали условие в котором сказали что если userData?.currentSong будет разна нулю и  userData?.currentSong.id будут строго не равны музыке под айди  то приминяется audio.currentTime = 0;  или же аудио с currentTime будет равно userData с songCurrentTime

--- main.js
+++ main.js
@@ -90,6 +90,11 @@ const playSong = (id) => {
   audio.src = song.src;
   audio.title = song.title;
 
+  if (userData?.currentSong === null || userData?.currentSong.id !== song.id) {
+    audio.currentTime = 0;
+  } else {
+    audio.currentTime = userData.songCurrentTime;
+  }
 
 };
 
