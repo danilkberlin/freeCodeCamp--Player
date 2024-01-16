3c30158ba293a0c87f9f158b8c5a71cc9b6e02c0
danilkberlin <danilkravets19@gmail.com>
2024-01-16 21:57:44 +0100

    step 50

--- main.js
+++ main.js
@@ -109,7 +109,6 @@ const pauseSong = () => {
 };
 
 const playNextSong = () => {
-
   if (userData?.currentSong === null) {
     playSong(userData?.songs[0].id);
   } else {
@@ -121,11 +120,22 @@ const playNextSong = () => {
 };
 
 const playPreviousSong = () => {
-  if (userData?.currentSong === null) return;
-  else {
+   if (userData?.currentSong === null) return;
+   else {
     const currentSongIndex = getCurrentSongIndex();
+    const previousSong = userData?.songs[currentSongIndex - 1];
 
-  }
+    playSong(previousSong.id);
+   }
+};
+
+const highlightCurrentSong = () => {
+  const playlistSongElements = document.querySelectorAll(".playlist-song");
+  const songToHighlight = document.getElementById(
+    `song-${userData?.currentSong?.id}`
+  );
+  
+
 };
 
 const renderSongs = (array) => {
@@ -164,5 +174,6 @@ pauseButton.addEventListener("click",  pauseSong);
 
 nextButton.addEventListener("click", playNextSong);
 
+previousButton.addEventListener("click", playPreviousSong);
+
 renderSongs(userData?.songs);
-const playlistSongElements = document.querySelectorAll('')
